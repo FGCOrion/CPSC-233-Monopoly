40ef71e6412a059600b7531cb31f2d428e4929cf
DanielJR346 <47425989+DanielJR346@users.noreply.github.com>
2019-02-11 10:43:15 -0700

    Update Game_Main.java

--- Game_Main.java
+++ Game_Main.java
@@ -1,4 +1,19 @@
 class Game_Main {
+  public int money;
+  public String name;
+  
+  Game_Main(Str string, int startingMoney) {
+    money = startingMoney;
+    name = str;
+  }
+  
+  public gainMoney(int num) {
+    money += num;
+  }
+  
+  public loseMoney(int num) {
+    money -= num;
+  }
 
 
 }
