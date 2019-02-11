2991fc63e20ff9a8d3a42a95a9b1d0a481aefbb6
alexs2112 <47425839+alexs2112@users.noreply.github.com>
2019-02-11 14:10:07 -0700

    Redid

--- GameMain.java
+++ GameMain.java
@@ -1,19 +1,4 @@
 class GameMain {
-  public int money;
-  public String name;
-  
-  GameMain(String name, int startingMoney) { //Constructor for initializing game
-    money = startingMoney;
-    name = name;
-  }
-  
-  public void gainMoney(int num) { //when a player lands on a property
-    money += num;
-  }
-  
-  public void loseMoney(int num) { // when a player lands on a property
-    money -= num;
-  }
-
-
+    public int player_money;
+    public int comp_money;
 }
