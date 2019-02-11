1546240f602e8265748b0471f6b392fe2116103d
HiddenBoxI <44384527+HiddenBoxI@users.noreply.github.com>
2019-02-12 01:49:18 +0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -3,5 +3,19 @@ class Player {
   * Instance variables: "piece" of type Piece, more to follow.
   * Setters and Getters for instance variables
   */
-
+  //instance variables
+  private Piece piece;
+  //getter method
+  public Piece getPiece()
+  {
+    return piece
+  }
+  //setter method
+  public void setPiece(Piece newPiece)
+  {
+    this.piece=newPiece;
+  }
+  
+  
+  
 }
