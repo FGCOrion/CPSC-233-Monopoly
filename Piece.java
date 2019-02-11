7c5fe7e6108f54999b7c52064885c2e1cf7456c5
Ryan Fowler <47425437+FGCOrion@users.noreply.github.com>
2019-02-11 10:37:28 -0700

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -1,5 +1,21 @@
 class Piece {
+    /* Instance Variables */
+    private int position = 0;
+    private char avatar;
 
+    /* Getters */
+    public int getXposition(){
+        return position;
+    }
+    public char getAvatar(){
+        return avatar;
+    }
 
+    /* Setters */
+    public void setPosition(int newPosition){
+        position = newPosition;
+    }
+    public void setAvatar(char avatarType){
+            avatar = avatarType;
 
 }
