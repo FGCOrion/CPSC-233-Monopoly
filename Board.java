a5b3181712cf066ec927c8df1e67c3d62afc0814
alexs2112 <47425839+alexs2112@users.noreply.github.com>
2019-02-11 13:44:19 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,7 +1,11 @@
 class Board {
-  /**
-  * Instance Variables: "spaces" of type ArrayList
-  * Setters and Getters for instance variables
-  */
+    /**
+    * Instance Variables: "spaces" of type ArrayList
+    * Setters and Getters for instance variables
+    */
+  
+    int boardArray[];
+    boardArray = new int[28];
+    //Creates a new array of integers with 28 elements (6 on each side + corners)
 
 }
