c30b5eaffa0fe8c1995bf1bd0ca22692f471fd91
HiddenBoxI <44384527+HiddenBoxI@users.noreply.github.com>
2019-02-12 02:02:51 +0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -5,16 +5,37 @@ class Player {
   */
   //instance variables
   private Piece piece;
+  
   //getter method
+  
   public Piece getPiece()
   {
     return piece
   }
   //setter method
+  
   public void setPiece(Piece newPiece)
   {
     this.piece=newPiece;
   }
+  
+  //constructor
+  
+  public Player()
+  {
+    
+  }
+  //constructors
+  public Player(Piece c)
+  {
+    this.piece=c;
+  }
+  
+  public Player(Piece k)//copy constructor
+  {
+    this.piece=k.piece;
+  }
+
   
   
   
