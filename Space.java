f23edb1ec8c95562d87778b795190e93c5448912
alexs2112 <47425839+alexs2112@users.noreply.github.com>
2019-02-11 20:01:42 -0700

    Update Board and Space
    Added a basic array and 7 really generic spaces
    Updated the space file slightly
    These compile although the copy constructor in player is kind of buggy

--- Space.java
+++ Space.java
@@ -12,12 +12,40 @@ class Space {
     public int owner;
     
     
-    //Basic constructor for a space
+    //Basic constructors for a space
+    public Space(String name,int cost, int value) {
+        this.name = name;
+        this.cost = cost;
+        this.value = value;
+        this.owner = 0;
+        //Defaults to nobody having any control of the space
+    }
+    
     public Space(String name,int cost, int value, int owner) {
         this.name = name;
         this.cost = cost;
         this.value = value;
         this.owner = owner;
+    }
+    
+    public void newOwner(int owner) {
+        this.owner = owner;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public int getCost() {
+        return this.cost;
+    }
+    
+    public int getValue() {
+        return this.value;
+    }
+    
+    public int getOwner() {
+        return this.owner;
     }
 
 }
