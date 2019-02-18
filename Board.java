b3e423ec738b285217035b81901dabc0ef77d46b
alexs2112 <47425839+alexs2112@users.noreply.github.com>
2019-02-18 11:12:52 -0700

    Updated Board Array, added a main method

--- Board.java
+++ Board.java
@@ -1,23 +1,29 @@
+import java.util.ArrayList;
+
 class Board {
     /**
     * Instance Variables: "spaces" of type ArrayList
     * Setters and Getters for instance variables
     */
-  
-    //Sets every space as a space object to add to the array of the board
-    Space space1 = new Space("Space 1", 150, 50);
-    Space space2 = new Space("Space 2", 200, 75);
-    Space space3 = new Space("?", 0, 0);
-    Space space4 = new Space("Space 4", 250, 100);
-    Space space5 = new Space("Space 5", 300, 125);
-    Space space6 = new Space("Jail", 350, 150);
     
-    Space[] boardArray = {space1, 
-                          space2,
-                          space3,
-                          space4,
-                          space5,
-                          space6,
-                         };
+    public Board() {
+    //Basic Constructor that builds the board
+        ArrayList<Space> boardArray = new ArrayList<Space>();
+    
+        //Sets every space as a space object to add to the array of the board
+        Space space1 = new Space("Space 1", 150, 50);
+        Space space2 = new Space("Space 2", 200, 75);
+        Space space3 = new Space("?", 0, 0);
+        Space space4 = new Space("Space 4", 250, 100);
+        Space space5 = new Space("Space 5", 300, 125);
+        Space space6 = new Space("Jail", 350, 150);
+        
+        boardArray.add(space1);
+        boardArray.add(space2);
+        boardArray.add(space3);
+        boardArray.add(space4);
+        boardArray.add(space5);
+        boardArray.add(space6);
+    }
 
 }
--- GameMain.java
+++ GameMain.java
@@ -1,3 +1,6 @@
 class GameMain {
-    
+    public static void main(String[] args) {
+        Board board = new Board();
+        //Constructs a new board
+    }
 }
