909d8ab818ddd500bfa68345b1275a137b406c50
alexs2112 <47425839+alexs2112@users.noreply.github.com>
2019-02-21 19:43:02 -0700

    Update GameMain.java
    Added a die rolling method (1 6 sided die) and set up a while loop and a timer (after 500 turns it ends)

--- GameMain.java
+++ GameMain.java
@@ -1,6 +1,21 @@
+import java.util.Random;
+
 class GameMain {
+	//Method to roll a single 6 sided die
+	private int rollDie() {
+		Random result = new Random();
+		return result.nextInt(6) + 1;
+	}
+	
     public static void main(String[] args) {
         Board board = new Board();
-        //Constructs a new board
+        //Constructs a new board	
+		
+		//totalTurns subtracts by 1 each time it goes through the loop until it hits 0
+		int totalTurns = 500;
+		
+		while (totalTurns > 0) {
+			
+		}
     }
 }
