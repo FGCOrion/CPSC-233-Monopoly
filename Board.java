1aa7d295ff1c28cfb2eb438884e855dd528a95a3
alexs2112 <47425839+alexs2112@users.noreply.github.com>
2019-02-25 17:40:19 -0700

    Update Board

--- Board.java
+++ Board.java
@@ -22,32 +22,32 @@ class Board {
         //Sets every space as a space object to add to the array of the board
 		Space space0 = new Space("GO", 0, 0, 3);
 		
-        Space space1 = new Space("Space 1", 150, 50); 		//33.3% income to cost
-        Space space2 = new Space("Space 2", 200, 75); 		//37.5%
-        Space space3 = new Space("?", 0, 0, 3);
-        Space space4 = new Space("Space 4", 250, 100);		//40.0%
-        Space space5 = new Space("Space 5", 300, 125);  	//41.7%
+        Space space1 = new Space("Yellow 1", 150, 50); 			//33.3% income to cost
+        Space space2 = new Space("Railroad", 200, 100); 		
+        Space space3 = new Space("Yellow 2", 200, 75);			//37.5%
+        Space space4 = new Space("Yellow 3", 250, 100);			//40.0%
+        Space space5 = new Space("Pick up a Card", 0, 0, 3);
         Space space6 = new Space("Jail", 0, 0, 3);
 		
-		Space space7 = new Space("Space 7", 400, 175);		//43.8%
-		Space space8 = new Space("Space 8", 450, 200);		//44.4%
-		Space space9 = new Space("?", 0, 0, 3);
-		Space space10 = new Space("Space 10", 500, 225);	//45.0%
-		Space space11 = new Space("Space 11", 550, 250);	//45.5%
+		Space space7 = new Space("Blue 1", 350, 150);		//42.9%
+		Space space8 = new Space("Blue 2", 400, 175);		//43.8%
+		Space space9 = new Space("Railroad", 200, 100);
+		Space space10 = new Space("Blue 3", 450, 200);		//44.4%
+		Space space11 = new Space("Income Tax", 0, 250, 3);
 		Space space12 = new Space("Free Parking", 0, 0, 3);
 		
-		Space space13 = new Space("Space 13", 650, 300);	//46.2%
-		Space space14 = new Space("Space 14", 700, 325);	//46.4%
-		Space space15 = new Space("?", 0, 0, 3);
-		Space space16 = new Space("Space 16", 750, 350);	//46.7%
-		Space space17 = new Space("Space 17", 800, 375);	//46.9%
+		Space space13 = new Space("Red 1", 550, 250);		//45.5%
+		Space space14 = new Space("Railroad", 200, 100);
+		Space space15 = new Space("Red 2", 600, 275);		//45.8%
+		Space space16 = new Space("Red 3", 650, 300);		//46.2%
+		Space space17 = new Space("Pick up a Card", 0, 0, 3);
 		Space space18 = new Space("Go to Jail", 0, 0, 3);
 		
-		Space space19 = new Space("Space 19", 900, 425);	//47.2%
-		Space space20 = new Space("Space 20", 950, 450);	//47.4%
-		Space space21 = new Space("?", 0, 0, 3);
-		Space space22 = new Space("Space 22", 1000, 475);	//47.5%
-		Space space23 = new Space("Space 23", 1100, 550);	//50.0%  This one is intentionally skewed
+		Space space19 = new Space("Orange 1", 750, 350);	//46.7%
+		Space space20 = new Space("Orange 2", 800, 375);	//46.9%
+		Space space21 = new Space("Community Fund", 0, 0, 3);
+		Space space22 = new Space("Green 1", 900, 425);		//47.2%
+		Space space23 = new Space("Green 2", 1000, 500);	//50.0%  This one is intentionally skewed
 		
 		
 		boardArray.add(space0);
