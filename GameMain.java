ea00eaede8c93ba0ca8361e69ce6f2a8175a1847
alexs2112 <47425839+alexs2112@users.noreply.github.com>
2019-02-26 13:11:46 -0700

    Moved playerTurn method to Player from GameMain

--- GameMain.java
+++ GameMain.java
@@ -43,51 +43,6 @@ class GameMain {
 			return false;
 	}
 	
-	//Method for the players turn
-	private static void playerTurn() {
-		Scanner input = new Scanner(System.in);
-		//Creates a new scanner
-		
-		print("\nIt is your turn");
-		int x = rollDie();
-		print("You rolled a " + String.valueOf(x));
-		int oldPosition = player.getPosition();
-		player.setPosition(oldPosition + x);
-		if (player.getPosition() > board.getLength()) {
-			print("You passed GO and collected $200");
-			player.setPosition(player.getPosition() - board.getLength());
-			player.setMoney(player.getMoney() + 200);
-		}
-		
-		Space newSpace = board.getSpace(player.getPosition() - 1);
-		print("You landed on " + newSpace.getName());
-		
-		//If the space the player lands on is unowned
-		if (newSpace.getOwner() == 0) {
-			print(newSpace.getName() + " is unowned. Would you like to purchase it for $" + String.valueOf(newSpace.getCost()) + "? (Value of $" + String.valueOf(newSpace.getValue()) + ")");
-			print("(y/n only please)");
-			char choice = ' ';
-			while (choice != 'y' && choice != 'n') {
-				choice = input.next().charAt(0);
-			}
-			if (choice == 'y') {
-				
-				//If the player has >= money to the spaces cost then they buy it
-				if (player.getMoney() >= newSpace.getCost()) {
-					print("Success");
-					player.setMoney(player.getMoney() - newSpace.getCost());
-					newSpace.setOwner(1);
-				}
-				else {
-					print("You cannot afford this space");
-				}
-			}		
-		}
-			
-	}
-
-	
-	
     public static void main(String[] args) {
 		
         Board board = new Board();
@@ -102,7 +57,7 @@ class GameMain {
 		while (endConditions(turn, player, computer) == false) {
 			roundStart(turn);
 				
-			playerTurn();
+			player.takeTurn(board);
 
 			turn += 1;
 		}
--- Player.java
+++ Player.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Random;
+
 class Player {
     /**
      * Instance variables: "piece" of type Piece, more to follow.
@@ -67,5 +70,59 @@ class Player {
         avatar = otherPlayer.getAvatar();
         money = otherPlayer.getMoney();
     }
+	
+	//Method to roll a single 7 sided die
+	private static int rollDie() {
+		Random result = new Random();
+		return result.nextInt(7) + 1;
+	}
+	
+	//Method to print text so I don't have to write System.out.print every single time
+	public static void print(String text) {
+		System.out.println(text);
+	}
+	
+	//Method for the players turn
+	public void takeTurn(Board board) {
+		Scanner input = new Scanner(System.in);
+		//Creates a new scanner
+		
+		print("\nIt is your turn");
+		int x = rollDie();
+		print("You rolled a " + String.valueOf(x));
+		int oldPosition = this.getPosition();
+		this.setPosition(oldPosition + x);
+		if (this.getPosition() > board.getLength()) {
+			print("You passed GO and collected $200");
+			this.setPosition(this.getPosition() - board.getLength());
+			this.setMoney(this.getMoney() + 200);
+		}
+		
+		Space newSpace = board.getSpace(this.getPosition() - 1);
+		print("You landed on " + newSpace.getName());
+		
+		//If the space the player lands on is unowned
+		if (newSpace.getOwner() == 0) {
+			print(newSpace.getName() + " is unowned. Would you like to purchase it for $" + String.valueOf(newSpace.getCost()) + "? (Value of $" + String.valueOf(newSpace.getValue()) + ")");
+			print("(y/n only please)");
+			char choice = ' ';
+			while (choice != 'y' && choice != 'n') {
+				choice = input.next().charAt(0);
+			}
+			if (choice == 'y') {
+				
+				//If the player has >= money to the spaces cost then they buy it
+				if (this.getMoney() >= newSpace.getCost()) {
+					print("Success");
+					this.setMoney(this.getMoney() - newSpace.getCost());
+					newSpace.setOwner(1);
+				}
+				else {
+					print("You cannot afford this space");
+				}
+			}		
+		}
+			
+	}
 
 }
