9fc38de375f0f78901ee4a1a0c9d06d8838795e6
brittpysar <43663838+brittpysar@users.noreply.github.com>
2019-03-13 23:23:01 -0600

    updated layout of board and player 1 and 2 info

--- Gui.java
+++ Gui.java
@@ -36,34 +36,37 @@ public class Gui extends Application {
 	private Text informationOfBox = new Text();
 	private GameMain turns=new GameMain();
 	//private ChoiceBox choiceBox = new ChoiceBox();
-	private Label text = new Label("Click square to get information");
-	private Label Explanation=new Label("Explanation: ");
+	//private Label turn = new Label("Your turn: P1");
+	private Label text = new Label("Click square for space information");
+	private Label Explanation=new Label("Space Details: ");
 	private Label resultOfDice=new Label();
 	private ComputerAI AI=new ComputerAI(2,false);
-    private Label Pmoney=new Label("P1's Money: 1500");
-    private Label Pproperty=new Label("P1's Property: 0");
-    private Label Pturn=new Label("Turn processing at: ");
+	private Label PInfo=new Label("Player One");
+    	private Label Pmoney=new Label("Money: 1500");
+    	private Label Pproperty=new Label("Properties owned: 0");
+    	private Label Pturn=new Label("Current Turn: ");
 	private Label Pavatar=new Label("A");
 	private Label Pcontrol=new Label("A");
-    private Label AImoney=new Label("P2's Money 1500: ");
-    private Label AIproperty=new Label("P2's Property: 0");
-    private Label AIinformation = new Label("Display message: ");
-    private Label Pposition = new Label("P1's Position: ");
-    private Label AIposition = new Label ("P2's Position: ");
+	private Label AIInfo=new Label("Player Two");
+   	private Label AImoney=new Label("Money: 1500");
+   	private Label AIproperty=new Label("Properties owned: ");
+    	private Label AIinformation = new Label("Display message: ");
+    	private Label Pposition = new Label("Current position: ");
+    	private Label AIposition = new Label ("Current position: ");
 	private Label AIavatar=new Label("B");
 	private Label AIcontrol=new Label("B");
-    private Label AIinformation2 = new Label(" "); // info about if go was passed
-    private Label AIinformation3 = new Label(" "); // info about what space was landed on
-    private Label AIinformation4 = new Label(" "); // info about price/rent
-    private Label AIinformation5 = new Label(" "); // info about if something was bought
-    private Label AIinformation1 = new Label(" "); // info about dice rolled
-    private Label AIinformation0 = new Label(" "); // info about whose turn it is
-    //private Label Pinformation0 = new Label();
-    //private Label Pinformation1 = new Label();
-    private Label status = new Label();
-    private int playerFlag = 1;
-    private int rollUnlocked = 1;
-    private int choiceUnlocked = 0;
+   	private Label AIinformation2 = new Label(" "); // info about if go was passed
+    	private Label AIinformation3 = new Label(" "); // info about what space was landed on
+    	private Label AIinformation4 = new Label(" "); // info about price/rent
+    	private Label AIinformation5 = new Label(" "); // info about if something was bought
+    	private Label AIinformation1 = new Label(" "); // info about dice rolled
+    	private Label AIinformation0 = new Label(" "); // info about whose turn it is
+    	//private Label Pinformation0 = new Label();
+    	//private Label Pinformation1 = new Label();
+    	private Label status = new Label();
+    	private int playerFlag = 1;
+    	private int rollUnlocked = 1;
+    	private int choiceUnlocked = 0;
 	String basicText = "";
 
 
@@ -147,18 +150,19 @@ public class Gui extends Application {
 	Pcontrol.setFont(Font.font ("Verdana", 8));
 	AIcontrol.setFont(Font.font ("Verdana", 8));
 	
-    root.add(Explanation,0,3,2,3);
-
-    root.add(new Label(""), 4, 20);
-    root.add(text,2,3,4,3);
+    root.add(Explanation,1,9,2,1);
+    //root.add(new Label(""), 4, 20);
+    root.add(text,1,10,5,1);
     root.add(resultOfDice,8,3,10,3);
-    root.add(Pmoney, 3, 4,4,4);
-    root.add(Pposition, 3,5,4,4);
-    root.add(Pproperty, 3,5,4,5);
-    root.add(Pturn, 3,6,4,6);
-    root.add(AImoney, 3,1,4,1);
-    root.add(AIposition, 3,2,4,1);
-    root.add(AIproperty, 3,2,4,2);
+	root.add(PInfo, 2, 0, 3, 1);
+    root.add(Pmoney, 1, 1, 3, 1);
+    root.add(Pposition, 1, 2 , 3, 1);
+    root.add(Pproperty, 1, 3, 3, 1);
+    root.add(Pturn, 9,0,3,1);
+	root.add(AIInfo, 2, 4, 3, 1);
+    root.add(AImoney, 1, 5, 3, 1);
+    root.add(AIposition, 1, 6, 3, 1);
+    root.add(AIproperty, 1, 7, 3, 1);
     root.add(AIinformation,6,6,7,6);
     root.add(AIinformation0,9,5,10,6);
     root.add(AIinformation1,9,6,10,5);
@@ -169,14 +173,7 @@ public class Gui extends Application {
     //root.add(Pinformation0,10,5,11,5);
     //root.add(Pinformation1,8,4,9,4);
     root.add(status,8,1,10,1);
-
-
-
-
-
-
-
-
+	//root.add(turn, 9, 3, 3, 1);
 
 
 		/**
@@ -235,7 +232,7 @@ public class Gui extends Application {
               setPlayerFlag(2);
               setRollUnlocked(1);
             }
-            Pposition.setText("P1's Position: " + (numOfLand.getSpace(gameGui.getPosition() - 1)).getName());
+            Pposition.setText("Current position: " + (numOfLand.getSpace(gameGui.getPosition() - 1)).getName());
           }
           else if(getPlayerFlag() == 2){
             setChoiceUnlocked(AI.takeTurnGui(numOfLand, gameGui, AIinformation0, AIinformation1, AIinformation2,
@@ -244,13 +241,13 @@ public class Gui extends Application {
               setPlayerFlag(1);
               setRollUnlocked(1);
             }
-            AIposition.setText("P2's Position: " + (numOfLand.getSpace(AI.getPosition() - 1)).getName());
+            AIposition.setText("Current position: " + (numOfLand.getSpace(AI.getPosition() - 1)).getName());
           }
-          Pmoney.setText("P1's Money: " + gameGui.getMoney());
-          AImoney.setText("P2's Money: " + AI.getMoney());
-          AIproperty.setText("P2's Property: " + AI.getPropertiesOwned());
-          Pproperty.setText("P1's Property: " + gameGui.getPropertiesOwned());
-          Pturn.setText("Turn Number: " + turnCount);
+          Pmoney.setText("Money: " + gameGui.getMoney());
+          AImoney.setText("Money: " + AI.getMoney());
+          AIproperty.setText("Properties owned: " + AI.getPropertiesOwned());
+          Pproperty.setText("Properties owned: " + gameGui.getPropertiesOwned());
+          Pturn.setText("Current Turn: " + turnCount);
         }
       }
     }
@@ -269,15 +266,15 @@ public class Gui extends Application {
         if(getChoiceUnlocked() == 1) {
           if (getPlayerFlag() == 1) {
             gameGui.purchase(numOfLand, AIinformation5);
-            Pmoney.setText("P1's Money: " + gameGui.getMoney());
+            Pmoney.setText("Money: " + gameGui.getMoney());
             setPlayerFlag(2);
-            Pproperty.setText("P1's Property: " + gameGui.getPropertiesOwned());
+            Pproperty.setText("Properties owned: " + gameGui.getPropertiesOwned());
           }
           else if (getPlayerFlag() == 2) {
             AI.purchase(numOfLand, AIinformation5);
-            AImoney.setText("P2's Money: " + AI.getMoney());
+            AImoney.setText("Money: " + AI.getMoney());
             setPlayerFlag(1);
-            AIproperty.setText("P2's Property: " + AI.getPropertiesOwned());
+            AIproperty.setText("Properties owned: " + AI.getPropertiesOwned());
           }
           setChoiceUnlocked(0);
           setRollUnlocked(1);
@@ -318,13 +315,13 @@ public class Gui extends Application {
     Button bt01 = new Button("Jail");
     bt01.setMaxWidth(150);
 	bt01.setMaxHeight(400);
-    root.add(bt01,6,0);
+    root.add(bt01,6,1);
     bt01.setOnAction(new EventHandler<ActionEvent>()
     {
       @Override
       public void handle(ActionEvent event) {
 
-        text.setText("Going to the jail");
+        text.setText("Going to jail");
         }
     }
   );
@@ -332,7 +329,7 @@ public class Gui extends Application {
     bt02.setMaxWidth(150);
 	bt02.setMaxHeight(400);
 	bt02.setStyle("-fx-background-color: #00F5FF");
-	root.add(bt02,7,0);
+	root.add(bt02,7,1);
 	if (gameGui.getSpaceOwned(7) == true)
 		root.add(Pcontrol,7,0);
 	if (AI.getSpaceOwned(7) == true)
@@ -357,7 +354,7 @@ public class Gui extends Application {
     bt03.setMaxWidth(150);
 	bt03.setMaxHeight(400);
 	bt03.setStyle("-fx-background-color: #00E5EE");
-    root.add(bt03,8,0);
+    root.add(bt03,8,1);
     bt03.setOnAction(new EventHandler<ActionEvent>()
     {
       @Override
@@ -375,7 +372,7 @@ public class Gui extends Application {
     Button bt04 = new Button("Rail\nRoad 2");
     bt04.setMaxWidth(150);
 	bt04.setMaxHeight(400);
-    root.add(bt04,9,0);
+    root.add(bt04,9,1);
     bt04.setOnAction(new EventHandler<ActionEvent>()
     {
       @Override
@@ -394,7 +391,7 @@ public class Gui extends Application {
     bt05.setMaxWidth(150);
 	bt05.setMaxHeight(400);
 	bt05.setStyle("-fx-background-color: #00F5FF");
-    root.add(bt05,10,0);
+    root.add(bt05,10,1);
     bt05.setOnAction(new EventHandler<ActionEvent>()
     {
       @Override
@@ -413,7 +410,7 @@ public class Gui extends Application {
     Button bt06 = new Button("Tax");
     bt06.setMaxWidth(150);
 	bt06.setMaxHeight(400);
-    root.add(bt06,11,0);
+    root.add(bt06,11,1);
     bt06.setOnAction(new EventHandler<ActionEvent>()
     {
       @Override
@@ -426,7 +423,7 @@ public class Gui extends Application {
     Button bt07 = new Button("Free\nPark");
     bt07.setMaxWidth(150);
 	bt07.setMaxHeight(400);
-    root.add(bt07,12,0);
+    root.add(bt07,12,1);
     bt07.setOnAction(new EventHandler<ActionEvent>()
     {
       @Override
@@ -441,7 +438,7 @@ public class Gui extends Application {
     bt11.setMaxWidth(150);
 	bt11.setMaxHeight(400);
 
-    root.add(bt11,6,1);
+    root.add(bt11,6,2);
     bt11.setOnAction(new EventHandler<ActionEvent>()
     {
       @Override
@@ -455,7 +452,7 @@ public class Gui extends Application {
     bt21.setMaxWidth(150);
 	bt21.setMaxHeight(400);
 	bt21.setStyle("-fx-background-color: #FFFF00");
-    root.add(bt21,6,2);
+    root.add(bt21,6,3);
     bt21.setOnAction(new EventHandler<ActionEvent>()
     {
       @Override
@@ -475,7 +472,7 @@ public class Gui extends Application {
     bt31.setMaxWidth(150);
 	bt31.setMaxHeight(400);
 	bt31.setStyle("-fx-background-color: #EEEE00");
-    root.add(bt31,6,3);
+    root.add(bt31,6,4);
     bt31.setOnAction(new EventHandler<ActionEvent>()
     {
       @Override
@@ -494,7 +491,7 @@ public class Gui extends Application {
     bt41.setMaxWidth(150);
 	bt41.setMaxHeight(400);
 
-    root.add(bt41,6,4);
+    root.add(bt41,6,5);
     bt41.setOnAction(new EventHandler<ActionEvent>()
     {
       @Override
@@ -512,7 +509,7 @@ public class Gui extends Application {
     Button bt51 = new Button();
     bt51.setMaxWidth(150);
 	bt51.setMaxHeight(400);
-    root.add(bt51,6,5);
+    root.add(bt51,6,6);
     bt51.setStyle("-fx-background-color: #FFFF00");
     bt51.setOnAction(new EventHandler<ActionEvent>()
     {
@@ -531,7 +528,7 @@ public class Gui extends Application {
     Button bt61 = new Button("Go");
     bt61.setMaxWidth(150);
 	bt61.setMaxHeight(400);
-    root.add(bt61,6,6);
+    root.add(bt61,6,7);
     bt61.setOnAction(new EventHandler<ActionEvent>()
     {
       @Override
@@ -547,7 +544,7 @@ public class Gui extends Application {
     bt17.setMaxWidth(150);
 	bt17.setMaxHeight(200);
 	bt17.setStyle("-fx-background-color: #FF0000");
-    root.add(bt17,12,1);
+    root.add(bt17,12,2);
     bt17.setOnAction(new EventHandler<ActionEvent>()
     {
       @Override
@@ -567,7 +564,7 @@ public class Gui extends Application {
     bt27.setMaxWidth(150);
 	bt27.setMaxHeight(400);
 	bt27.setStyle("-fx-background-color: #EE0000");
-    root.add(bt27,12,2);
+    root.add(bt27,12,3);
     bt27.setOnAction(new EventHandler<ActionEvent>()
     {
       @Override
@@ -585,7 +582,7 @@ public class Gui extends Application {
     Button bt37 = new Button("Rail\nRoad 3");
     bt37.setMaxWidth(150);
 	bt37.setMaxHeight(400);
-    root.add(bt37,12,3);
+    root.add(bt37,12,4);
 
     bt37.setOnAction(new EventHandler<ActionEvent>()
     {
@@ -606,7 +603,7 @@ public class Gui extends Application {
 	bt47.setMaxHeight(400);
 	bt47.setStyle("-fx-background-color: #FF0000");
 
-    root.add(bt47,12,4);
+    root.add(bt47,12,5);
     bt47.setOnAction(new EventHandler<ActionEvent>()
     {
       @Override
@@ -624,7 +621,7 @@ public class Gui extends Application {
     Button bt57 = new Button("Chance");
     bt57.setMaxWidth(150);
 	bt57.setMaxHeight(400);
-    root.add(bt57,12,5);
+    root.add(bt57,12,6);
     bt57.setOnAction(new EventHandler<ActionEvent>()
     {
       @Override
@@ -638,7 +635,7 @@ public class Gui extends Application {
     Button bt67 = new Button("Go To\nJail");
     bt67.setMaxWidth(150);
 	bt67.setMaxHeight(400);
-    root.add(bt67,12,6);
+    root.add(bt67,12,7);
     bt67.setOnAction(new EventHandler<ActionEvent>()
     {
       @Override
@@ -654,7 +651,7 @@ public class Gui extends Application {
     bt72.setMaxWidth(150);
 	bt72.setMaxHeight(400);
 	bt72.setStyle("-fx-background-color: #7CFC00");
-    root.add(bt72,7,6);
+    root.add(bt72,7,7);
     bt72.setOnAction(new EventHandler<ActionEvent>()
     {
       @Override
@@ -672,7 +669,7 @@ public class Gui extends Application {
     Button bt73 = new Button();
     bt73.setMaxWidth(150);
 	bt73.setMaxHeight(400);
-    root.add(bt73,8,6);
+    root.add(bt73,8,7);
     bt73.setStyle("-fx-background-color: #00FA9A");
     bt73.setOnAction(new EventHandler<ActionEvent>()
     {
@@ -691,7 +688,7 @@ public class Gui extends Application {
     Button bt74 = new Button("Comm\nFund");
     bt74.setMaxWidth(150);
 	bt74.setMaxHeight(400);
-    root.add(bt74,9,6);
+    root.add(bt74,9,7);
     bt74.setOnAction(new EventHandler<ActionEvent>()
     {
       @Override
@@ -705,7 +702,7 @@ public class Gui extends Application {
     bt75.setMaxWidth(150);
 	bt75.setMaxHeight(400);
 	bt75.setStyle("-fx-background-color: #FFD700");
-    root.add(bt75,10,6);
+    root.add(bt75,10,7);
     bt75.setOnAction(new EventHandler<ActionEvent>()
     {
       @Override
@@ -724,7 +721,7 @@ public class Gui extends Application {
     bt76.setMaxWidth(150);
 	bt76.setMaxHeight(400);
 	bt76.setStyle("-fx-background-color: #EEC900");
-    root.add(bt76,11,6);
+    root.add(bt76,11,7);
     bt76.setOnAction(new EventHandler<ActionEvent>()
     {
       @Override
